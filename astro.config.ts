1c1af07e6dcf29ffc0016c7f43cb3483aee59dde
Johann Ohler <johannohler0@gmail.com>
2026-02-06 10:29:10 -0800

    drop useless files and minor changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,59 +1,32 @@
-import fs from "node:fs";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import { defineConfig } from "astro/config";
 
-import { expressiveCodeOptions } from "./src/site.config";
 import { siteConfig } from "./src/site.config";
-
-// Remark plugins
-import remarkDirective from "remark-directive";/* Handle ::: directives as nodes */
-import remarkUnwrapImages from "remark-unwrap-images";
-import { remarkAdmonitions } from "./src/plugins/remark-admonitions";/* Add admonitions */
-import { remarkReadingTime } from "./src/plugins/remark-reading-time";
-
-// Rehype plugins
-import rehypeExternalLinks from "rehype-external-links";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-      domains: ["webmention.io"],
-	},
-
   integrations: [
-      expressiveCode(expressiveCodeOptions),
       icon(),
       tailwind({
           applyBaseStyles: false,
           nesting: true,
       }),
       sitemap(),
-      mdx(),
       robotsTxt(),
       webmanifest({
-          // See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
-          /**
-           * required
-           **/
           name: siteConfig.title,
-          /**
-           * optional
-           **/
-          // short_name: "Astro_Cactus",
           description: siteConfig.description,
           lang: siteConfig.lang,
-          icon: "public/icon.svg", // the source for generating favicon & icons
+          icon: "public/icon.svg",
           icons: [
               {
-                  src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+                  src: "icons/apple-touch-icon.png",
                   sizes: "180x180",
                   type: "image/png",
               },
@@ -78,54 +51,11 @@ export default defineConfig({
               insertManifestLink: false,
           },
       }),
-	],
+  ],
 
-  markdown: {
-      rehypePlugins: [
-          [
-              rehypeExternalLinks,
-              {
-                  rel: ["nofollow, noreferrer"],
-                  target: "_blank",
-              },
-          ],
-      ],
-      remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkDirective, remarkAdmonitions],
-      remarkRehype: {
-          footnoteLabelProperties: {
-              className: [""],
-          },
-      },
-	},
-
-  // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
 
-  // ! Please remember to replace the following site property with your own domain
   site: "https://johannohler.com/",
-
-  vite: {
-      optimizeDeps: {
-          exclude: ["@resvg/resvg-js"],
-      },
-      plugins: [rawFonts([".ttf", ".woff"])],
-	},
 
   adapter: netlify()
 });
-
-function rawFonts(ext: string[]) {
-    return {
-        name: "vite-plugin-raw-fonts",
-        // @ts-expect-error:next-line
-        transform(_, id) {
-            if (ext.some((e) => id.endsWith(e))) {
-                const buffer = fs.readFileSync(id);
-                return {
-                    code: `export default ${JSON.stringify(buffer)}`,
-                    map: null,
-                };
-            }
-        },
-    };
-}
